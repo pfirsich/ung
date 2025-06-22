ae96728cd80c02cd4f290bc6ab665b3c7f831cee
Joel Schumacher <joelschum@gmail.com>
2025-06-22 18:07:39 +0200

    Move state to shared header

---
+++ src/state.hpp
@@ -0,0 +1,103 @@
+#pragma once
+
+#include "types.hpp"
+#include "um.h"
+#include "ung/core.h"
+
+#include <SDL.h>
+
+struct fastObjMesh;
+
+namespace ung {
+struct UConstant {
+    um_vec4 screen_dimensions; // xy: size, zw: reciprocal size
+};
+
+struct UFrame {
+    um_vec4 time; // x: seconds since game started, y: frame counter
+};
+
+struct UCamera {
+    um_mat view;
+    um_mat view_inv;
+    um_mat projection;
+    um_mat projection_inv;
+    um_mat view_projection;
+    um_mat view_projection_inv;
+};
+
+struct UTransform {
+    um_mat model;
+    um_mat model_inv;
+    um_mat model_view;
+    um_mat model_view_projection;
+};
+
+struct Transform {
+    um_mat local_matrix;
+    um_quat orientation;
+    um_vec3 position;
+    um_vec3 scale;
+    SlotMap::Key parent;
+    SlotMap::Key first_child;
+    SlotMap::Key prev_sibling;
+    SlotMap::Key next_sibling;
+    mugfx_uniform_data_id uniform_data;
+    bool local_matrix_dirty;
+};
+
+struct Material {
+    mugfx_material_id material;
+    mugfx_uniform_data_id constant_data;
+    mugfx_uniform_data_id dynamic_data;
+    StaticVector<mugfx_draw_binding, 16> bindings;
+};
+
+struct Camera {
+    um_mat projection;
+    um_mat projection_inv;
+    ung_transform_id transform;
+};
+
+struct GeometryData {
+    fastObjMesh* mesh;
+    usize num_vertices;
+    usize num_indices;
+};
+
+constexpr usize MaxMouseButtons = 16;
+
+struct InputState {
+    std::array<bool, SDL_NUM_SCANCODES> key_down;
+    std::array<u8, SDL_NUM_SCANCODES> key_pressed;
+    std::array<u8, SDL_NUM_SCANCODES> key_released;
+    int mouse_x, mouse_y, mouse_dx, mouse_dy;
+    int mouse_scroll_left, mouse_scroll_right, mouse_scroll_y_pos, mouse_scroll_y_neg;
+    std::array<bool, MaxMouseButtons> mouse_button_down;
+    std::array<u8, MaxMouseButtons> mouse_button_pressed;
+    std::array<u8, MaxMouseButtons> mouse_button_released;
+};
+
+struct State {
+    Pool<Transform> transforms;
+    Pool<Material> materials;
+    Pool<Camera> cameras;
+    Pool<GeometryData> geometry_data;
+    UConstant u_constant;
+    UFrame u_frame;
+    UCamera u_camera;
+    mugfx_uniform_data_id constant_data;
+    mugfx_uniform_data_id frame_data;
+    mugfx_uniform_data_id camera_data;
+    SDL_Window* window;
+    SDL_GLContext context;
+    ung_event_callback event_callback = nullptr;
+    void* event_callback_ctx = nullptr;
+    u32 win_width;
+    u32 win_height;
+    InputState input;
+};
+
+extern State* state;
+
+}
