86b4496c4ead9f21d652eabaa976694aa9458d3a
Joel Schumacher <joelschum@gmail.com>
2025-10-04 16:21:13 +0200

    Prefix some cmake functions

--- cmake/no-exceptions.cmake
+++ cmake/no-exceptions.cmake
@@ -1,4 +1,4 @@
-function(set_no_exceptions target)
+function(ung_set_no_exceptions target)
   if(MSVC)
     # There is a way to do this for a single target, but it's very annoying, so I just don't
     # string(REGEX REPLACE "/EHsc" "/EHs-c-" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
--- cmake/no-rtti.cmake
+++ cmake/no-rtti.cmake
@@ -1,4 +1,4 @@
-function(set_no_rtti target)
+function(ung_set_no_rtti target)
   if(MSVC)
     # There is a way to do this for a single target, but it's very annoying, so I just don't
     # string(REGEX REPLACE "/GR" "/GR-" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
